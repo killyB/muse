1814c8dd95c29d90ba697e7c6251df7425873bbb
killyb <mike.kilberry@gmail.com>
2015-03-24 01:55:54 -0500

    image pull working & image resizing on index page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,3 +41,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
+
+Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.9.0-Q16'
+Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
