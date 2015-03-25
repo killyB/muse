77032d19c9e3168245093d3079976aeda5b5e051
killyb <mike.kilberry@gmail.com>
2015-03-25 00:40:08 -0500

    figured out auth to Twitter with Env var

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter::REST::Client.new do |config|
-  config.consumer_key = "G3Xp8jrXuLASCuMUXHkcdt2YH"
-  config.consumer_secret = "0Wz3cuibK07i25TE0ilgY58CLnnU7QTcQZmv9KvWU7MNS6rYen"
-  config.access_token = "3112886296-eVSeaV8kxFZwdsO2hp5ji3wafMTLcJ0RofyBeqZ"
-  config.access_token_secret = "SUPyDKRhEkab00jhsSnsxv93nnYsp61xO6MEtG0hiZEHX"
+      config.consumer_key = ENV['TWITTER_KEY']
+      config.consumer_secret = ENV['TWITTER_SECRET']
+      config.access_token = ENV['TWITTER_TOKEN']
+      config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
 end
